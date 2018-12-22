e7a30b7dfef81c50466e1deeac7cab6f6f9c642b
Simon <therealslimv@yahoo.de>
2018-12-22 21:41:25 +0100

    implemented the observable class

--- modules/view/observable.py
+++ modules/view/observable.py
@@ -1,26 +1,30 @@
+from typing import List
+
+from modules.view.subscriber import Subscriber
+
 
 ##  Class for creating updating strings
 #
 #   Can be used if you want to create an string in the view, that will be updated with new values
-from modules.view.subscriber import Subscriber
-
-
 class Observable:
+
+    subscribers: List[Subscriber] = []
 
     ##  Method to trigger an update on all subscribers
     #
     #   @param update the new value
     def next(self, update: str) -> None:
-        pass
+        for subscriber in self.subscribers:
+            subscriber.update(update)
 
     ##  A subscriber can be added with this function
     #
     #   @param subscriber the subscriber that wants to receive status updates
     def add_subscriber(self, subscriber: Subscriber) -> None:
-        pass
+        self.subscribers.append(subscriber)
 
     ##  A subscriber can be removed with this function
     #
     #   @param subscriber the subscriber who wants to be removed from receiving status updates
     def remove_subscriber(self, subscriber: Subscriber) -> None:
-        pass
+        self.subscribers.remove(subscriber)

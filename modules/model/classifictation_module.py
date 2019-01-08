9f710551e008c4b99cfda3d2f5eac3414db31c74
Simon <therealslimv@yahoo.de>
2019-01-08 14:47:05 +0100

    Set up the structure for the classification module

---
+++ modules/model/classifictation_module.py
@@ -0,0 +1,34 @@
+from modules.shared.matrix import Matrix
+
+
+##  This class handles the classification of matrices using a neural network
+class Classifier:
+
+    __path: str = ""
+
+    ##  Starts the classification process
+    #
+    #   @param path where the matrix that will be classified is located
+    #   @param network path where the neural network is located
+    @staticmethod
+    def start(path: str, network: str):
+        pass
+
+    @staticmethod
+    def __print():
+        pass
+
+    ##  Load the neural network
+    #
+    #   @param network path where the neural network is located
+    @staticmethod
+    def __load_network(network: str):
+        pass
+
+    ##  Scales all the values of a matrix into a fixed range
+    #
+    #   @param matrix which will be normalized
+    #   @return matrix which is normalized
+    @staticmethod
+    def __normalize(matrix: Matrix) -> Matrix:
+        pass

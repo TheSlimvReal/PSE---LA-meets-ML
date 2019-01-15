7b289405d752885640ab0a05de90ebb958b4f665
yannickfunk <uvieb@student.kit.edu>
2019-01-15 12:03:23 +0100

    reduced test matrices amount and removed last row of csv

--- modules/test/unittest/model/collector_module/test_collector.py
+++ modules/test/unittest/model/collector_module/test_collector.py
@@ -4,8 +4,8 @@ from modules.model.collector_module.collector import Collector
 
 
 def test_collect():
-    data = Collector.collect(30, 1, 1, "1", "1")
-    assert len(data) == 30
+    data = Collector.collect(5, 1, 1, "1", "1")
+    assert len(data) == 5
     for matrix in data:
         plt.spy(matrix.todense())
         plt.show()

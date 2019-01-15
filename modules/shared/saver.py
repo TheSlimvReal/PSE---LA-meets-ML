e53fd88c1919752ae896a55d32c0d0b1fb9ed257
yannickfunk <uvieb@student.kit.edu>
2019-01-15 15:44:31 +0100

    added the saver which stores the matrices from a given list into an hdf5 file(dense format, sparse format to come)

--- modules/shared/saver.py
+++ modules/shared/saver.py
@@ -1,4 +1,4 @@
-
+import h5py
 
 ##  Class that handles the saving of datasets
 class Saver:
@@ -10,4 +10,12 @@ class Saver:
     #   @param path where it will be saved
     @staticmethod
     def save(dataset, name: str, path: str) -> None:
-        pass
+        saving_file = h5py.File(path+name+'.hdf5', 'w')
+        group = saving_file.create_group('dense_matrices')
+        id_counter = 0
+        for matrix in dataset:
+            group.create_dataset('matrix_num_'+str(id_counter), data = matrix.todense())
+            id_counter += 1
+
+
+

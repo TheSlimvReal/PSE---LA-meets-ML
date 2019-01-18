ed7130f0a5fd916ddb6fd91f08cfa7f4b995fccd
Unknown <dennis.groetzinger@web.de>
2019-01-18 19:20:06 +0100

    pycharm integration working

--- Prototypes/Collector/p.py
+++ Prototypes/Collector/p.py
@@ -3,11 +3,26 @@ import os
 from ctypes import *
 #charptr = POINTER(c_char)
 #os.system('scp -r ugsqo@lsdf-28-131.scc.kit.edu:/tmp/pycharm_project_213/Prototypes/Collector/testlibrary.so C:/Users/Dennis/Documents/uni/PSE/Project/PSE---LA-meets-ML/Prototypes/Collector')
+#import os
+#os.environ['CXX'] = '/usr/local/bin/g++-6.4'
+#os.environ['CC'] = '/usr/local/bin/gcc-6.4'
+#os.environ['PATH'] = os.getenv("PATH","fail")+ ':/usr/local/cuda-9.0/bin/'
+#os.environ['LD_LIBRARY_PATH'] = '/usr/local/cuda/lib'
+#os.environ['LD_LIBRARY_PATH'] = os.getenv("LD_LIBRARY_PATH","fail")+':/usr/local/lib'
+#os.environ['LD_LIBRARY_PATH'] = os.getenv("LD_LIBRARY_PATH","fail")+':/usr/local/lib64'
+
+#os.system('$CXX test.cpp -shared -fpic -I/usr/local/include/ -L/usr/local/lib/-lginkgo_omp -lginkgo_reference -lginkgo -lginkgo_cuda -o testlibrary3.so')
+
 mutable_string = create_string_buffer(str.encode("real"))
+
+
+
+
+
 #mystr1=c_char_p("mystring")
 a = 1
 
-test = CDLL('/home/ugsqo/home/ugsqo/Prototypes/Collector/testlibrary4.so')
+test = CDLL('/home/ugsqo/home/ugsqo/Prototypes/Collector/testlibrary3.so')
 #test.blub.argtypes = []
 #test.initializetest.restype = charptr
 #test._Z4blubNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEE.argtypes = [charptr]
--- Prototypes/Collector/test.py
+++ Prototypes/Collector/test.py
@@ -9,6 +9,9 @@ import os
 #os.system('$CXX test.cpp -shared -fpic -I/usr/local/include/ -L/usr/local/lib/-lginkgo_omp -lginkgo_reference -lginkgo -lginkgo_cuda -o testlibrary3.so')
 
 
+
+#in theory, if you do not have a remote interpreter this should work as well(if you update the paths accordingly)
+# because this will just set the environment that way, if you 
 os.environ['CXX'] = '/usr/local/bin/g++-6.4'
 os.environ['CC'] = '/usr/local/bin/gcc-6.4'
 os.environ['PATH'] = os.getenv("PATH","fail")+ ':/usr/local/cuda-9.0/bin/'
@@ -16,9 +19,11 @@ os.environ['LD_LIBRARY_PATH'] = '/usr/local/cuda/lib'
 os.environ['LD_LIBRARY_PATH'] = os.getenv("LD_LIBRARY_PATH","fail")+':/usr/local/lib'
 os.environ['LD_LIBRARY_PATH'] = os.getenv("LD_LIBRARY_PATH","fail")+':/usr/local/lib64'
 
-
+#os.system will execute in a subcommand, that is why export does not work
+#environ is the environment of the current python script which is why it does not reset imedietly
 #os will, in my understanding, generate a new sub shell in which the environment variables will be set
 #thats why one can not just run p.py since it will be in another environment
+os.system('python3.6 p.py')
 os.system('python3.6 p.py')
 #print(os.system('pwd'))
 #print(os.environ)

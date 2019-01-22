03ca6f503586abd68347ee1a02bc833e318655f2
yannickfunk <uvieb@student.kit.edu>
2019-01-22 13:12:53 +0100

    changed for saver for also labeled data and set up a training example

--- modules/model/training_module/training_module.py
+++ modules/model/training_module/training_module.py
@@ -1,3 +1,11 @@
+import h5py
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import optimizers
+
 
 
 ##  This class handles the training of the neural network
@@ -12,4 +20,37 @@ class TrainingModule:
     #   @param saving_path path to where the trained network will be saved
     @staticmethod
     def train(matrices_path: str, neural_network_path: str, name: str, saving_path: str):
-        pass
+        NUM_CLASSES = 5
+        model = Sequential()
+
+        model.add(Conv2D(32, (3, 3), input_shape=(128, 128, 1)))
+        model.add(Activation('relu'))
+
+        model.add(Flatten())
+
+        model.add(Dense(64))
+        model.add(Activation('relu'))
+
+        model.add(Dense(NUM_CLASSES))
+        model.add(Activation('sigmoid'))
+
+        optimizer = optimizers.SGD(lr=0.001)
+        model.compile(loss='categorical_crossentropy',
+                     optimizer=optimizer,
+                     metrics=['accuracy'])
+
+
+        ################################
+        datagen = ImageDataGenerator()
+        dataset = h5py.File(matrices_path, 'r')
+
+
+        matrices = np.expand_dims(np.array(dataset['dense_matrices']), axis=3)
+        labels = np.array(dataset['label_vectors'])
+
+        model.fit_generator(datagen.flow(matrices, labels, batch_size=25),
+                            steps_per_epoch=len(matrices) / 25, epochs=1)
+
+
+
+

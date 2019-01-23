9f61a45acb6b08539a09ba0afb86611c2aa1b86c
Simon <therealslimv@yahoo.de>
2019-01-23 15:58:12 +0100

    implemented config file parsing

--- modules/shared/configurations.py
+++ modules/shared/configurations.py
@@ -18,4 +18,20 @@ class Configurations:
 
     @staticmethod
     def get_collector_config(key: Key) -> str or int:
-        return Configurations.__data["collect"][Configurations.__mapping[key]]
+        return Configurations.__read_from_config("controller", key)
+
+    @staticmethod
+    def get_label_config(key: Key) -> str or int:
+        return Configurations.__read_from_config("label", key)
+
+    @staticmethod
+    def get_train_config(key: Key) -> str or int:
+        return Configurations.__read_from_config("train", key)
+
+    @staticmethod
+    def get_classify_config(key: Key) -> str or int:
+        return Configurations.__read_from_config("classify", key)
+
+    @staticmethod
+    def __read_from_config(module: str, key: Key) -> str or int:
+        return Configurations.__data[module][Configurations.__mapping[key]]

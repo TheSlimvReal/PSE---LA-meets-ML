b71346ba7485f96c330d255e030b8bff9717b2f0
Simon <therealslimv@yahoo.de>
2019-01-26 13:18:59 +0100

    added another test for the configuration fetching

--- modules/test/unittest/shared/test_configurations.py
+++ modules/test/unittest/shared/test_configurations.py
@@ -11,3 +11,9 @@ def test_loading_config_values_works():
         data = json.load(f)
     expected = data["label"]["saving-path"]
     assert actual == expected
+
+
+def test_loading_config_has_right_value():
+    actual = Configurations.get_config_with_key(Module.TRAIN, Key.TRAIN)
+    expected = 0.3
+    assert actual == expected

9b640267039153e58659a885010fc2ea318ded08
Unknown <dennis.groetzinger@web.de>
2019-01-28 10:30:55 +0100

    remove Prototypes

--- Prototypes/LabelingModule/pro/run.py
+++
@@ -1,9 +0,0 @@
-from modules.controller.controller import Controller
-
-
-##  Run this script to start the command line interaction
-#
-#   Example interaction:
-#       collect -a 5 -n example_matrices -p modules/shared/data/
-#       quit
-Controller().start_interaction()

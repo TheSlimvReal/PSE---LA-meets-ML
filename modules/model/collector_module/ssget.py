6d5ef5cdb9ee2ddb3ad64f40f5859b2f8da8e9fe
annaric <anna.ricker8@gmail.com>
2019-01-30 18:27:47 +0100

    fixed collect output problems

--- modules/model/collector_module/ssget.py
+++ modules/model/collector_module/ssget.py
@@ -33,7 +33,7 @@ class SSGet:
     @staticmethod
     def __download_matrix(size: int) -> np.ndarray:
         matrix_id = random.choice(SSGet.__real_square_matrices_ids)
-        download_command = "ssget -e -i " + matrix_id + " -t mat"
+        download_command = "ssget -e -i " + matrix_id + " -t mat 2>/dev/null"
         path = os.popen(download_command).read().strip()    # just an example
         if SSGet.__load(path) == []:
             return []

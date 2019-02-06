61617c8dd605bee5e159a78381ee07b0614c7a3b
yannickfunk <yannickfunk@yahoo.de>
2019-02-06 14:38:28 +0100

    pep

--- modules/model/classification_module/classification_module.py
+++ modules/model/classification_module/classification_module.py
@@ -26,22 +26,19 @@ class Classifier:
         predictions = list(np.argmax(model.predict(matrix), axis=1))
         Classifier.__print(predictions)
 
-
     @staticmethod
     def __print(predictions: list):
         counter = 0
         for prediction in predictions:
-            print("matrix: "+str(counter)+", predicted solver: "+Classifier.__solvers[prediction])
+            print("matrix: " + str(counter) + ", predicted solver: " + Classifier.__solvers[prediction])
             counter += 1
-
 
     ##  Load the neural network
     #
     #   @param network path where the neural network is located
     @staticmethod
     def __load_network(network: str):
-         return keras.models.load_model(network)
-
+        return keras.models.load_model(network)
 
     ##  Scales all the values of a matrix into a fixed range
     #

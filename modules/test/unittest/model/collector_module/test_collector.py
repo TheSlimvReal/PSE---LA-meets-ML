ac13aac25680159501fb2f7c274aa62ffceae6e7
annaric <anna.ricker8@gmail.com>
2019-02-11 19:09:14 +0100

    deleted matrix print in collector test

--- modules/test/unittest/model/collector_module/test_collector.py
+++ modules/test/unittest/model/collector_module/test_collector.py
@@ -9,9 +9,6 @@ import numpy as np
 def test_collect():
     data = Collector.collect(5, 128, 'unlabeled_matrices', 'modules/shared/data/')
     assert len(data) == 5
-    #for matrix in data:
-     #  plt.spy(matrix.todense())
-       # plt.show()
     created_file = h5py.File('modules/shared/data/unlabeled_matrices.hdf5', 'r')
     print(created_file['dense_matrices'])
 

3aed24093806ffc9a50c4aafd7d7a0cc13400e27
annaric <anna.ricker8@gmail.com>
2019-02-11 19:08:28 +0100

    added test print override with incomplete input

--- modules/test/unittest/view/test_cli_output_service.py
+++ modules/test/unittest/view/test_cli_output_service.py
@@ -13,9 +13,27 @@ def test_create_observable_to_print_three_values(mocked_cli):
         75,
         100
     ]
+    expected_calls = [call.print_overriding("downloading %s values" % str(i)) for i in values]
+    obs = Observable()
+    output_service = CLIOutputService(mocked_cli)
+    output_service.print_stream("downloading %s values", obs)
+    [obs.next(str(i)) for i in values]
+    mocked_cli.assert_has_calls(expected_calls)
+
+
+@patch("modules.view.command_line_interface.CommandLineInterface")
+def test_print_overriding_with_incomplete_input(mocked_cli):
+    values = [
+        0,
+        25,
+        50,
+        75,
+        100
+    ]
     expected_calls = [call.print_overriding("downloading %s" % str(i)) for i in values]
     obs = Observable()
     output_service = CLIOutputService(mocked_cli)
-    output_service.print_stream("downloading %s", obs)
+    output_service.print_stream("downloading", obs)
     [obs.next(str(i)) for i in values]
     mocked_cli.assert_has_calls(expected_calls)
+

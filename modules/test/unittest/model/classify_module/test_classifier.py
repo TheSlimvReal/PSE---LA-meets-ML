330c6ed121e2e50fd7ab2dea803c095d09c1d348
annaric <anna.ricker8@gmail.com>
2019-02-20 13:15:59 +0100

    loader now throws exceptions

--- modules/test/unittest/model/classify_module/test_classifier.py
+++ modules/test/unittest/model/classify_module/test_classifier.py
@@ -4,14 +4,15 @@ from modules.exception.exceptions import IllegalArgumentException
 from modules.model.classification_module.classification_module import Classifier
 
 
-@patch("modules.exception.exceptions.IllegalArgumentException")
+@patch("modules.exception.exceptions.MyException.__init__")
 @patch("modules.view.cli_output_service.CLIOutputService")
 @patch("h5py.File")
 @patch("modules.shared.regularity_calculator.RegularityCalculator.is_regular")
 def test_classify_throws_error_if_matrix_is_irregular(mocked_validator, mocked_loader, mocked_cli, mocked_exception):
     Classifier.set_output_service(mocked_cli)
+    mocked_loader.side_effect = ["matrix_file", "key", "matrix"]
     mocked_validator.side_effect = [False]
     exception = IllegalArgumentException
-    mocked_exception.__init__.side_effect = [exception]
+    mocked_exception.return_value = IllegalArgumentException
     Classifier.start("path", "network")
     mocked_cli.has_calls([call.print_error(exception)])

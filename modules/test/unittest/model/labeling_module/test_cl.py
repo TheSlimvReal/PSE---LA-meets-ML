46d572835995018466028117cfc93e6d8311b415
Fabian Koffer <naibafreffok@googlemail.com>
2019-02-21 15:01:29 +0100

    added a test for cl, checking wether the attributes are correctly passed

--- modules/test/unittest/model/labeling_module/test_cl.py
+++ modules/test/unittest/model/labeling_module/test_cl.py
@@ -2,9 +2,14 @@ from mock import patch
 from modules.model.labeling_module import cl
 
 
-@patch("modules.model.labeling_module.labeling_module.start", return_value=None)
+@patch("modules.model.labeling_module.labeling_module.LabelingModule.start")
 def test_cl(mock_labeling_module):
-    cl.start("modules/shared/data/UnlabeledMatrices/unlabeled_matrices.hdf5", "testLocation",
-             "modules/test/unittest/shared/data/")
+    def mocked_labeling_module_start(path: str, saving_name: str, saving_path: str):
+        assert path == "testPath"
+        assert saving_name == "testSavingName"
+        assert saving_path == "testSavingPath"
+
+    mock_labeling_module.side_effect = mocked_labeling_module_start
+    cl.start("testPath", "testSavingName", "testSavingPath")
 
 

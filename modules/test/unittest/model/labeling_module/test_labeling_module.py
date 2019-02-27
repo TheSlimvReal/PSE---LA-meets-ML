96d982d6b15f917d31a97dc659b6c16c638a8d50
Unknown <dennis.groetzinger@web.de>
2019-02-27 16:20:11 +0100

    ll

--- modules/test/unittest/model/labeling_module/test_labeling_module.py
+++ modules/test/unittest/model/labeling_module/test_labeling_module.py
@@ -4,6 +4,25 @@ import numpy as np
 from modules.model.labeling_module.labeling_module import LabelingModule
 from modules.model.labeling_module.ginkgo import Ginkgowrapper
 
+"""
+from mock import patch, call
+import scipy
+import numpy as np
+
+from modules.model.labeling_module import labeling_module
+
+@patch("modules.labeling_module_ginkgo.calculate_time_to_solve")
+def test_controller_with_two_iterations(mocked_ginkgo_py):
+    mocked_ginkgo_py.side_effects = 3
+    matrix = np.random((128,128))
+    sparse_matrix = scipy.sparse.csr_matrix(matrix)
+    returnV = labeling_module.calculate_label(sparse_matrix)
+
+    print(returnV)
+
+"""
+
+
 
 @patch("modules.model.labeling_module.ginkgo.Ginkgowrapper.__init__", return_value=None)
 @patch("modules.model.labeling_module.ginkgo.Ginkgowrapper.calculate_time_to_solve", return_value=1)

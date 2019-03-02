000563c6448d2cf35fe30fa0906cfd8140d1fdc3
Simon <therealslimv@yahoo.de>
2019-03-02 15:21:55 +0100

    added tests for entering wrong format inputs

--- modules/test/integrationtest/test_controller.py
+++ modules/test/integrationtest/test_controller.py
@@ -1,6 +1,7 @@
-from mock import patch
+from mock import patch, call
 
 from modules.controller.controller import Controller
+from modules.exception.exceptions import IllegalArgumentException
 
 
 @patch("modules.controller.commands.label_command.LabelCommand.execute")
@@ -53,3 +54,35 @@ def test_train_command_call(mocked_input, mocked_train):
     con = Controller()
     con.start_interaction()
     mocked_train.assert_called_once()
+
+
+@patch("modules.view.command_line_interface.CommandLineInterface.print")
+@patch("builtins.input")
+def test_wrong_integer_format_prints_error_message(mocked_input, mocked_print):
+    user_input = [
+        "collect -n name -a 50.000",
+        "quit"
+    ]
+    expected_calls = [
+        call(IllegalArgumentException("").get_type() + ": 50.000 is not a integer"),
+        call("Finished")
+    ]
+    mocked_input.side_effect = user_input
+    Controller().start_interaction()
+    mocked_print.assert_has_calls(expected_calls)
+
+
+@patch("modules.view.command_line_interface.CommandLineInterface.print")
+@patch("builtins.input")
+def test_wrong_float_format_prints_error_message(mocked_input, mocked_print):
+    user_input = [
+        "train --train 0,72 -n name -p path",
+        "quit"
+    ]
+    expected_calls = [
+        call(IllegalArgumentException("").get_type() + ": 0,72 is not a float"),
+        call("Finished")
+    ]
+    mocked_input.side_effect = user_input
+    Controller().start_interaction()
+    mocked_print.assert_has_calls(expected_calls)

e2ac7865cd33c0fcee8ac02a1e3afb3422f352b7
Fabian Koffer <naibafreffok@googlemail.com>
2019-03-11 15:05:50 +0100

    corrected earlier path mistake

--- modules/test/unittest/model/labeling_module/test_labeling_module.py
+++ modules/test/unittest/model/labeling_module/test_labeling_module.py
@@ -14,10 +14,11 @@ def test_labeling_module_returns_valid_times(mocked_ginkgo_calculate, mocked_gin
 
     labels = h5py.File("modules/test/unittest/shared/data/testLocation.hdf5", 'r')["label_vectors"]
     times = h5py.File("modules/test/unittest/shared/data/testLocation.hdf5", 'r')["calculated_times"]
-
-    times = np.array(times).tolist()
-    index = times.index(min(times))
-    assert(labels[index] == 1, "did not get the right minimum")
+    label_list = np.array(labels).tolist()
+    for i in range(len(labels)):
+        times_for_one_label = np.array(times[i]).tolist()
+        index_of_fastest_solver = times_for_one_label.index(min(times_for_one_label))
+        assert(label_list[i][index_of_fastest_solver] == 1.0), "did not generate the correct label"
 
 
 @patch("modules.model.labeling_module.ginkgo.Ginkgowrapper.__init__", return_value=None)
@@ -26,7 +27,7 @@ def test_labeling_module_saves_at_the_right_location(mocked_ginkgo_calculate, mo
     LabelingModule.start("modules/test/unittest/shared/data/test_unlabeled_matrices.hdf5", "testLocation",
                          "modules/test/unittest/shared/data/")
 
-    matrices = h5py.File("data/UnlabeledMatrices/unlabeled_matrices.hdf5", 'r')["dense_matrices"]
+    matrices = h5py.File("modules/test/unittest/shared/data/test_unlabeled_matrices.hdf5", 'r')["dense_matrices"]
     saved_matrices = h5py.File("modules/test/unittest/shared/data/testLocation.hdf5", 'r')["dense_matrices"]
     for i in range(len(matrices)):
         assert(np.array_equal(matrices[i], saved_matrices[i]))
@@ -43,6 +44,5 @@ def test_labeling_module_each_label_is_a_vector_with_one_one_and_zeros(mocked_gi
     for label in label_list:
         assert (label.count(1) == 1), "more or less 1s than 1 in a label"
         assert (label.count(0) == len(label) - 1), "more or less 0s in a label than expected"
-
 
 

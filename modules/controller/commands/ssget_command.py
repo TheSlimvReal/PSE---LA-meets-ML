18fb4c719dcf7e8a39ca5322af05a0405e48115f
annric <anna.ricker8@gmail.com>
2019-03-12 14:04:10 +0100

    added ssget -h (in progress)

--- modules/controller/commands/ssget_command.py
+++ modules/controller/commands/ssget_command.py
@@ -8,12 +8,15 @@ from modules.model.collector_module.ssget import SSGet
 #   this command will be created when entering ssget in the terminal
 #   @extends Command to use its parsing logic
 class SSGETCommand(Command):
-
     def __init__(self):
         super().__init__()
         self.valid_arguments = {
-            ("u", "update"): Key.UPDATE
+            ("u", "update"): Key.UPDATE,
+            ("h", "help"): Key.HELP,
         }
+        self.help_arguments = (
+            "-u updates the csv file, where all the matrix ID's of squared matrices from Suite Sparse are listed",
+        )
 
     def execute(self) -> None:
         super().execute()
